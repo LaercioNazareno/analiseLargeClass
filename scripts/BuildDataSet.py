d5a3a6d70ecd76ad93280dbd4b82d50e886d0c22
LaercioNazareno <laercio.nazareno1@gmail.com>
2022-03-13 20:10:23 +0000

    Mudanca de estrategia

--- scripts/BuildDataSet.py
+++
@@ -1,1 +0,0 @@
-projectsName = GetNameProjects()
--- scripts/GetProjects.py
+++ scripts/GetProjects.py
@@ -11,12 +11,12 @@ apiUrlBase = 'https://api.github.com/graphql'
 
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'bearer f4fb4aa95200cc25e360cd9d1a195a36460e1a67',
+    'Authorization': 'bearer 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',
 }
-## Verificar a ordem das estrelas
+
 query = """
     query {
-        search(query:"stars:>100 language:java" , type:REPOSITORY, first:100) {
+        search(query:"stars:>100 language:java" , type:REPOSITORY, first:5) {
             edges {
                 node{
                     ... on Repository {
@@ -34,9 +34,11 @@ query = """
         }
     }
 """
+
 json = {
     "query": query, "variables": {}
 }
+
 def getDataList():
     response = requests.post(apiUrlBase, headers=headers, json=json)
     data = response.json()['data']['search']['edges']
@@ -51,8 +53,56 @@ def dataProcessing(data):
         repository = node['node']
         nameProject = repository['name']
         link = repository['url']
-        command = "CloneProjects.bat "+ link + " " + nameProject +" "+ str(numProject)
-        os.system(command)
+        cloneProject(numProject, nameProject, link)
+        createRequiredFiles(nameProject)
+        avaliarComPmd(nameProject, numProject)
+        os.system("cls")
+
+def createRequiredFiles(nameProject):
+   createProjectFile(nameProject)
+   createClasspathFile(nameProject)
+
+def createClasspathFile(nameProject):
+    file = open("../repositories/"+nameProject+".classpath", 'w')
+    file.write("""<?xml version="1.0" encoding="UTF-8"?>
+<classpath>
+	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.7"/>
+	<classpathentry kind="output" path="bin"/>
+</classpath>""")
+    file.close()
+
+def createProjectFile(nameProject):
+    file = open("../repositories/"+nameProject+".project", 'w')
+    file.write("""<?xml version="1.0" encoding="UTF-8"?>
+<projectDescription>
+	<name>"""+nameProject+"""</name>
+	<comment></comment>
+	<projects>
+	</projects>
+	<buildSpec>
+		<buildCommand>
+			<name>org.eclipse.jdt.core.javabuilder</name>
+			<arguments>
+			</arguments>
+		</buildCommand>
+	</buildSpec>
+	<natures>
+		<nature>org.eclipse.jdt.core.javanature</nature>
+	</natures>
+</projectDescription>""")
+    file.close()
+
+def cloneProject(numProject, nameProject, link):
+    print("Clonando o projeto: "+ nameProject+" index: "+ str(numProject))
+    command = "git clone "+link+" ../repositories/"+nameProject
+    os.system(command)
+
+def avaliarComPmd(nameProject, numProject):
+    print("Avaliando o projeto "+nameProject+" index: "+ str(numProject))
+    diretory = "../results/"+nameProject
+    os.makedirs(diretory)
+    command = "pmd -d " +"../repositories/"+nameProject +" -f text  -R category/java/design.xml/GodClass -language java > "+ "../results/"+nameProject+"/pmdResultGodClass.txt"
+    os.system(command)
 
 def saveData(data):
     file = open("../results/evaluatedProjects.csv", 'w')
@@ -71,7 +121,7 @@ def saveData(data):
                          "Data de Atualizacao": updatedAt,
                          "Qtd estrelas": qtdEstrelas
                         })
-#adicionar o numero de estrelas 
+
 def openFileWriterAndWriteHeader(file):
     fieldnames = ["Nome","url","Data Criacao","Data de Atualizacao", "Qtd estrelas"]
     writer = csv.DictWriter(file, fieldnames=fieldnames)
@@ -82,8 +132,8 @@ data = getDataList()
 saveData(data)
 dataProcessing(data)
 
-# Decore 96 * 5
+# Decore não ok
 # PMD ok
-# Jdeodorant
-# Jspiriti não ok
+# Jdeodorant ok
+# Jspiriti ok
 # Multiview
